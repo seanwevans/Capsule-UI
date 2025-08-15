dc81635078b6ebe622857eafb57e3bc84408166d
Sean Evans <sean.w.evans@gmail.com>
2025-08-15 18:25:33 -0400

    Allow union type schemas and update sample tokens

--- dist/tokens.d.ts
+++ dist/tokens.d.ts
@@ -1,6 +1,6 @@
 export type ThemeName = 'light' | 'dark';
-export type TokenName = '--color-background' | '--color-text' | '--color-brand' | '--spacing-sm' | '--spacing-md' | '--spacing-lg';
-export type TokenValues = Record<ThemeName, string>;
+export type TokenName = '--color-background' | '--color-brand' | '--color-text' | '--spacing-lg' | '--spacing-md' | '--spacing-sm';
+export type TokenValues = Record<ThemeName, string | number>;
 export const tokens: Record<TokenName, TokenValues>;
 export default tokens;
 
