e8a60321493bc10825335c3045a40ebd557c2883
Sean Evans <sean.w.evans@gmail.com>
2025-09-27 14:27:35 -0400

    Update ThemeManager load return type

--- packages/core/theme-manager.d.ts
+++ packages/core/theme-manager.d.ts
@@ -1,7 +1,7 @@
 export class ThemeManager {
   static register(tenant: string, variables: Record<string, string | number>): void;
   static registerTheme(tenant: string, theme: string, variables: Record<string, string | number>): void;
-  static load(tenant: string, url: string): Promise<void>;
+  static load(tenant: string, url: string): Promise<boolean>;
   static apply(tenant: string, element?: HTMLElement): void;
   static applyTheme(tenant: string, theme: string, element?: HTMLElement): void;
   static unregister(tenant: string): void;
